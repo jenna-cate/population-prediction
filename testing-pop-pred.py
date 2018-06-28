337135f1ca82188a82da0248193f5b381adb380d
Jenna <33360492+jenna-cate@users.noreply.github.com>
2018-06-27 20:31:25 -0500

    Update testing-pop-pred.py

--- testing-pop-pred.py
+++ testing-pop-pred.py
@@ -12,6 +12,10 @@ from matplotlib import pyplot as plt
 
 data = pd.read_csv("trimmed_pop_data.csv")
 
+usadata = data[246:247]
+usapred = usadata[usadata.columns[-10:]]
+usapred = np.array(usapred)
+
 ## create workable dictionary ##
 frame = {}
 for i in range(0, len(data['Country'])):   ## build dict: frame containing a dict for every country which will hold all features as keyed lists
@@ -76,13 +80,13 @@ validation = {
     'Y': np.array(target[10000:])
 }
 
-X_train = training['X'] #.astype(np.float32)
-y_train = training['Y'] #.astype(np.float32)
-X_test = validation['X'] #.astype(np.float32)
-y_test = validation['Y'] #.astype(np.float32)
+X_train = training['X'] 
+y_train = training['Y'] 
+X_test = validation['X'] 
+y_test = validation['Y'] 
 
-params = {'n_estimators': 500, 'max_depth': 2, 'random_state': 10,
-          'learning_rate': 0.01, 'loss': 'ls'}
+params = {'n_estimators': 500, 'max_depth': 4, 'random_state': 10,
+          'learning_rate': 0.05, 'loss': 'ls'}
 est = ensemble.GradientBoostingRegressor(**params)
 
 est.fit(X_train, y_train)
@@ -90,13 +94,28 @@ trainpreds = est.predict(X_train)
 trmse = mean_squared_error(y_train, trainpreds)
 testpreds = est.predict(X_test)
 mse = mean_squared_error(y_test, testpreds)
+rmse = np.sqrt(mse)
+trrmse = np.sqrt(trmse)
+print("RMSE: %.4f" % rmse)
+print("TrRMSE: %.4f" % trrmse)
+ratio = rmse/trrmse
+print("Ratio test rmse:training rmse %.4f" % ratio)
 
-print("MSE: %.4f" % mse)
-print("TRMSE: %.4f" % trmse)
-ratio = mse/trmse
-print("Ratio test mse:training mse %.4f" % ratio)
+USApredictions = []
+usalead = usapred
+q=0
+while q < 10:
+    usapred = usapred.reshape(1, -1)
+    pred = est.predict(usapred)
+    USApredictions = np.append(USApredictions, pred)
+    usapred = np.delete(usapred, 0)
+    usapred = np.append(usapred, pred)
+    q+=1
+USApredictions = USApredictions.reshape(1, -1)
+print(USApredictions)
 
-
-
-
-
+plotdata = np.concatenate((usalead, USApredictions), axis = 0)
+plotdata = plotdata.flatten()
+years = np.arange(2007, 2027)
+plt.plot(years, plotdata, 'g*--')
+plt.show()
